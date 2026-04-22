2ca6601dd22abce0a31c4e2f399d18fe533d1e23
GuiRodriSil <rodriguesgui560@gmail.com>
2026-04-22 15:52:32 -0300

    mudanca do caminho base para o deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: "/Res03-Squad03-Agape_Sistemas-Frontend/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
